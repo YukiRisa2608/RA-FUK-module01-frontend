b5b361f7f84e0fa697f4391e98fac1c9c082a139
YukiRisa2608 <nham26081992@gmail.com>
2023-09-10 21:15:14 +0900

    session09 array

---
+++ thuc_hanh_modul01/form_dang_ki/validator.js
@@ -0,0 +1,109 @@
+//đối tượng Validator, đặt tên obj là options
+function Validator(options) {
+
+        var selectorRules = {};
+    //Hàm thực hiện validate
+    function validate(inputElement, rule) {
+        var errorElement = inputElement.parentElement.querySelector('.form-message');
+        var errorMessage;
+        //Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+        //Lặp qua từng rule và kiểm tra
+        //Nếu có lỗi thì dừng việc kiểm tra
+        for (var i = 0; i < rules.length; ++i) {
+            errorMessage = rules[i](inputElement.value);
+            if (errorMessage) break;
+        }
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+    }
+
+    //Lấy element của form cần validate
+    //lấy ra form element
+    //option.rules là một array, để gọi được các phần tử trong mảng dùng foreach
+    var formElement = document.querySelector(options.form);
+
+    if (formElement) {
+        formElement.onsubmit = function (e) {
+            //Khi submit form
+            e.preventDefault();
+        }
+
+        //Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
+        options.rules.forEach(function (rule) {
+            //Lưu lại các rules cho mỗi input
+             if (Array.isArray(selectorRules[rule.selector])){
+                selectorRules[rule.selector].push(rule.test);
+             } else {
+                selectorRules[rule.selector] = [rule.test];
+             }
+            //lấy input của element fullname và mail trong form
+            var inputElement = formElement.querySelector(rule.selector);
+
+            if (inputElement) {
+                //Xử lí trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+
+                }
+                //Xử lí mỗi khi người dùng nhập vào input sẽ ẩn thông báo lỗi đi
+                inputElement.oninput = function () {
+                    var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid');
+                }
+            }
+        });
+        console.log(selectorRules);
+    }
+}
+//chấm tới phương thức isRequied và gán cho nó một funtion để định nghĩa các rules
+//return một obj có key là selector, value là selector
+//trả thêm một function test để kiểm tra xem người dùng đã nhập chưa,
+//cho người dùng biết như thế nào là bắt buộc nhập, thì sẽ viết trong function này
+//với email cũng làm tương tự như vậy, nhưng để kiểm tra xem đã nhập đúng định dạng mail hay chưa
+//nguyên tắc rules
+//1. khi có lối trả ra message lỗi
+//2. khi hợp lệ không trả ra gì cả (undefined)
+Validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.trim() ? undefined : message || 'Vui lòng nhập trường này'
+        }
+    };
+}
+
+Validator.isEmail = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : message || 'Trường này phải là email';
+        }
+    };
+}
+//Xử lí mật khẩu
+Validator.minLength = function (selector, min, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length >= min ? undefined : message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+        }
+    };
+}
+//Xử lí mật khẩu nhập lại
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập không chính xác';
+        }
+    }
+}
